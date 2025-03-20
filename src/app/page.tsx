5767cf377c57d350b72e7270ee03d23289a975ae
jacob-s-codes <jacobbshaul@gmail.com>
2025-03-20 13:52:19 -0700

    added a test page that kinda works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 
 
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="mt-36 ml-8">
-        <h2 className="text-5xl font-extrabold"><span className="text-melon">Pokemon</span> or <span className="text-white">Software?</span></h2>
+    <div className="h-screen bg-black">
+      <div className="pt-36 ml-8">
+        <h2 className="text-9xl font-extrabold text-center"><span className="text-white">Pokemon</span> <span className="text-gray-200">or Software?</span></h2>
       </div>
     </div>
   );
