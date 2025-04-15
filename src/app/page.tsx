e0a40833ab6dd8510c428b71f9f82b7a2c126157
jacob-s-codes <jacobbshaul@gmail.com>
2025-04-15 12:52:17 -0700

    added logo for button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
       </div>
       <div className="mt-36 w-full flex justify-center items-center gap-x-12">
         <button className="border-2 border-black hover:bg-blue hover:cursor-pointer hover:text-white px-4 py-2 rounded-lg text-2xl uppercase" id="pokemon"><img src="/pokemonlogo.png" alt="" className="w-32"/></button>
-        <button className="border-2 border-black px-4 py-2 rounded-lg text-2xl uppercase" id="software">Software</button>
+        <button className="border-2 border-black px-4 py-2 hover:bg-blue hover:cursor-pointer hover:text-white rounded-lg text-2xl uppercase" id="software"><img src="/wwwlogo.png" alt="" className="w-32"/></button>
       </div>
 
       
